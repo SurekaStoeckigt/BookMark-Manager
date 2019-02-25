d233d43fbd1cdc63e81395903ca56939b4207d18
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-02-25 15:48:21 +0000

    first feature test passing

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,6 +1,14 @@
 feature 'Viewing bookmarks' do
+
   scenario 'displays message on index page' do
     visit('/')
     expect(page).to have_content "Bookmark Manager"
+  end
+
+  scenario 'displays content of the bookmarks route' do
+    visit('/bookmarks')
+    expect(page).to have_content "http://www.makersacademy.com" 
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
   end
 end
